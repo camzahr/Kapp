a50088f165c3b4b42db655f8c12019c154b94840
camzahr <camiller@ece.fr>
2015-03-18 12:10:53 -0600

    Routeursfinish

--- src/Kapp/PlatformBundle/Controller/AdvertController.php
+++ src/Kapp/PlatformBundle/Controller/AdvertController.php
@@ -7,16 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
-  {
-    $content = $this
-    ->get('templating')
-    ->render('KappPlatformBundle:Advert:index.html.twig', array(
-        'nom' => 'Jeremy'
-    )
-		);
-    return new Response($content);
-  }
+   public function indexAction()
+    {
+        // On veut avoir l'URL de l'annonce d'id 5.
+        $url = $this->get('router')->generate(
+            'kapp_platform_view', // 1er argument : le nom de la route
+            array('id' => 5)    // 2e argument : les valeurs des paramètres
+        );
+        // $url vaut « /platform/advert/5 »
+
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+    }
 
 
   // La route fait appel à OCPlatformBundle:Advert:view,
